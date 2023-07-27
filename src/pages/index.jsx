e2ac5589b42dc2f4ca387cf8f076cc6534002958
Aditya Kulkarni <adityakulkarni@AK-MacBook-Pro.local>
2023-07-26 19:06:47 -0700

    Updated base component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,25 +6,3 @@ export default function HomePage() {
     </div>
   );
 }
-
-// export default function HomePage() {
-//   const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
-//   const [likes, setLikes] = useState(0);
-
-//   function handleClick() {
-//     setLikes(likes + 1);
-//   }
-
-//   return (
-//     <div>
-//       <Header title="Develop. Preview. Ship. 🚀" />
-//       <ul>
-//         {names.map((name) => (
-//           <li key={name}>{name}</li>
-//         ))}
-//       </ul>
-
-//       <button onClick={handleClick}>Like ({likes})</button>
-//     </div>
-//   );
-// }
