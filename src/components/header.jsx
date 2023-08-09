996600e45f2c56675a3dc42230165895aea1490b
Aditya Kulkarni <adityakulkarni@AK-MacBook-Pro.local>
2023-08-08 19:43:12 -0700

    updated header file

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,25 @@
-function Header({ name }) {
-    return (
-      <h1>
-        {name}
-      </h1>
-      );
-  }
+import Link from "next/link";
+
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/">Experience</Link>
+      </li>
+      <li>
+        <Link href="/">Projects</Link>
+      </li>
+      <li>
+        <Link href="/">Skills</Link>
+      </li>
+      <li>
+        <Link href="/">Contact</Link>
+      </li>
+    </ul>
+  );
+}
 
 export default Header;
